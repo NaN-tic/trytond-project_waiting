1be6a431368eab3feb47d6c7c0ed08e89cec92b5
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:34:48 +0100

    Cleanup tests

--- tests/test_project_waiting.py
+++ tests/test_project_waiting.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class ProjectWaitingTestCase(unittest.TestCase):
-    '''
-    Test Project Waiting module.
-    '''
+    'Test Project Waiting module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('project_waiting')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('project_waiting')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
