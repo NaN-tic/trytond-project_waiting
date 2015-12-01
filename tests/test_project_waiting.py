074a920802c9171128688f684fab48f53c35d3c4
resteve <resteve@zikzakmedia.com>
2015-12-01 11:17:00 +0100

    Tests

--- tests/test_project_waiting.py
+++ tests/test_project_waiting.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the project_waiting module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProjectWaitingTestCase(unittest.TestCase):
+class ProjectWaitingTestCase(ModuleTestCase):
     'Test Project Waiting module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('project_waiting')
-
-    def test0005views(self):
-        'Test views'
-        test_view('project_waiting')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'project_waiting'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProjectWaitingTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
